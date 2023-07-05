f2ba5ea4fe5a772b77898610066dd93c7b55a1a1
Anastasia Kulesh <kulesh.anastasia.s@gmail.com>
2023-07-05 17:39:52 +0300

    feat: add popup to create a card

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,24 @@
 const page = document.querySelector('.page');
 const popup = page.querySelector('.popup');
-const popupForm = popup.querySelector('.popup__form');
+
+// Редактирование профиля
+const popup1 = page.querySelector('.popup_type_editProfile');
+const popupFormEditProfile = popup1.querySelector('[name="editProfileForm"]');
 const editButton = page.querySelector('.profile__button-edit');
-const closeButton = popup.querySelector('.popup__button-close');
-const saveButton = popupForm.querySelector('.popup__button-save');
+const closeButton = popup1.querySelector('.popup__button-close');
+const saveButton = popupFormEditProfile.querySelector('.popup__button-save');
 
 let infoProfileName = page.querySelector('.profile__name');
 let infoProfileDescription = document.querySelector('.profile__description');
-let popupInfoInputName = popupForm.querySelector('.popup__field_type_name');
-let popupInfoInputDescription = popupForm.querySelector('.popup__field_type_descriprion');
+let popupInfoInputName = popupFormEditProfile.querySelector('.popup__field_type_name');
+let popupInfoInputDescription = popupFormEditProfile.querySelector('.popup__field_type_descriprion');
+
+// Добавление карточки
+const popup2 = page.querySelector('.popup_type_add-card');
+const popupFormAddCard = popup2.querySelector('[name="addCard"]');
+const addButton = page.querySelector('.profile__button-add');
+const closeButtonAddCard = popup2.querySelector('.popup__button-close');
+const saveButtonAddCard = popupFormAddCard.querySelector('.popup__button-save');
 
 // Функция открытия popup
 function openPopup(popup) {
@@ -33,13 +43,13 @@ function handleFormSubmit (event) {
 }
 
 // Открытие popup по нажатию кнопки
-editButton.addEventListener('click', () => openPopup(popup));
+editButton.addEventListener('click', () => openPopup(popup1));
 
 // Закрытие popup по нажатию кнопки
-closeButton.addEventListener('click', () => closePopup(popup));
+closeButton.addEventListener('click', () => closePopup(popup1));
 
 // Сохранение inputs и закрытие popup
-popupForm.addEventListener('submit', handleFormSubmit); 
+popupFormEditProfile.addEventListener('submit', handleFormSubmit); 
 
 // Массив данных начальных карточек
 const initialCards = [
@@ -89,3 +99,10 @@ function renderCard(name, link) {
 initialCards.forEach(function(item) {
   renderCard(item.name, item.link)
 });
+
+
+// Открытие popup Добавления карточки по нажатию кнопки
+addButton.addEventListener('click', () => openPopup(popup2));
+
+// Закрытие popup Добавления карточки по нажатию кнопки
+closeButtonAddCard.addEventListener('click', () => closePopup(popup2));
