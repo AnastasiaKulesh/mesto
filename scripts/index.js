571e13cd63f698df9b30528c5bcad7b5837a4c30
Anastasia Kulesh <kulesh.anastasia.s@gmail.com>
2023-07-06 17:19:08 +0300

    refactor: code refactoring

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,31 @@
+// Массив данных начальных карточек
+const initialCards = [
+  {
+    name: 'Санкт-Петербург',
+    link: 'https://images.unsplash.com/photo-1643718249935-cb148dc74b76?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80'
+  },
+  {
+    name: 'Выборг',
+    link: 'https://images.unsplash.com/photo-1594652071880-3484b086d143?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80'
+  },
+  {
+    name: 'Великий Новгород',
+    link: 'https://images.unsplash.com/photo-1636622407062-7b9ea2967a75?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=770&q=80'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://images.unsplash.com/photo-1552735855-557bdba3961a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=778&q=80'
+  },
+  {
+    name: 'Роза Хутор',
+    link: 'https://images.unsplash.com/photo-1650622174543-7a2599f5e970?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80'
+  },
+  {
+    name: 'Алтай',
+    link: 'https://images.unsplash.com/photo-1635538365672-95c0ea660111?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=776&q=80'
+  }
+];
+
 const page = document.querySelector('.page');
 const popup = page.querySelector('.popup');
 
@@ -11,6 +39,10 @@ let infoProfileName = page.querySelector('.profile__name');
 let infoProfileDescription = document.querySelector('.profile__description');
 let popupInfoInputName = popupFormEditProfile.querySelector('.popup__field_type_name');
 let popupInfoInputDescription = popupFormEditProfile.querySelector('.popup__field_type_descriprion');
+
+// Template карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const cardsList = document.querySelector('.cards-grid__list');
 
 // Добавление карточки
 const popup2 = page.querySelector('.popup_type_add-card');
@@ -54,38 +86,6 @@ closeButton.addEventListener('click', () => closePopup(popup1));
 
 // Сохранение inputs и закрытие popup
 popupFormEditProfile.addEventListener('submit', handleFormSubmit); 
-
-
-// Массив данных начальных карточек
-const initialCards = [
-  {
-    name: 'Санкт-Петербург',
-    link: 'https://images.unsplash.com/photo-1643718249935-cb148dc74b76?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80'
-  },
-  {
-    name: 'Выборг',
-    link: 'https://images.unsplash.com/photo-1594652071880-3484b086d143?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80'
-  },
-  {
-    name: 'Великий Новгород',
-    link: 'https://images.unsplash.com/photo-1636622407062-7b9ea2967a75?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=770&q=80'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://images.unsplash.com/photo-1552735855-557bdba3961a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=778&q=80'
-  },
-  {
-    name: 'Роза Хутор',
-    link: 'https://images.unsplash.com/photo-1650622174543-7a2599f5e970?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80'
-  },
-  {
-    name: 'Алтай',
-    link: 'https://images.unsplash.com/photo-1635538365672-95c0ea660111?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=776&q=80'
-  }
-];
-
-const cardTemplate = document.querySelector('#card-template').content;
-const cardsList = document.querySelector('.cards-grid__list');
 
 // Создание карточки из массива
 function renderCard(name, link) {
